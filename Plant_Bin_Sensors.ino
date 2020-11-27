bf829230224c040318e8772cf1b1d07613c0e9dc
seanwolfgang <seanwolfgang.wachtel@gmail.com>
2020-11-27 15:15:37 -0700

    Added RTC module.

--- Plant_Bin_Sensors.ino
+++ Plant_Bin_Sensors.ino
@@ -10,10 +10,17 @@
 #include <Arduino.h>
 #include <hp_BH1750.h>  //  include the library
 
+// RTC library
+#include <DS3231.h>
+
 // Declaring class variables for sensors
 Adafruit_HTS221 hts;    // Temperature humidity
 Adafruit_seesaw ss;     // Soil
 hp_BH1750 BH1750;       // Light
+
+// Initialize the DS3231 using the hardware interface
+DS3231  rtc(SDA,SCL);
+
 
 void setup() {
   // Start serial monitor at 9600
@@ -27,6 +34,9 @@ void setup() {
   bool soil_avail   = ss.begin(0x36);
   bool BH1750_avail = BH1750.begin(BH1750_TO_GROUND);
 
+  // Initalize the RTC
+  rtc.begin();
+  
   // Check if all the sensors initialized
   if (!HTS221_avail || !soil_avail || !BH1750_avail) {
 
@@ -48,10 +58,10 @@ void setup() {
 }
 
 void loop() {
-  
-  
+  Serial.println(rtc.getTimeStr());
 }
 
+// Takes a boolean input and returns the string of what it is
 String booleanOutput(bool booleanInput) {
   if (booleanInput) {
     return "True";
@@ -60,6 +70,7 @@ String booleanOutput(bool booleanInput) {
   }
 }
 
+// Blink the built in LED defined in setup with the specified delay
 void blinkWithDelay(int millisecondDelay) {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(millisecondDelay);           // wait for a delay
